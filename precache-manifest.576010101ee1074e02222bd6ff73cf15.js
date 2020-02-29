408a6cbbaadd8989352cfa4366b8149fe407282b
Pygee <toni.pylvalainen@vr.fi>
2020-02-29 12:45:04 +0200

    Updates

---
+++ precache-manifest.576010101ee1074e02222bd6ff73cf15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9930be473d3008b333d7c63df0feca15",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "81d283b6e2a7b575f768",
+    "url": "/monsters/static/css/main.a8e7ce14.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81d283b6e2a7b575f768",
+    "url": "/monsters/static/js/main.9de9445c.chunk.js"
+  },
+  {
+    "revision": "27edc721a646a18481ad",
+    "url": "/monsters/static/js/runtime-main.9b332845.js"
+  }
+]);
